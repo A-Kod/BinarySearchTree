318d48a416aa22ff7fbe544406dd16df0ec1fdd8
A-Kod <andrey_kodyk@mail.ru>
2016-09-22 13:02:05 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+#include "BinarySearchTree.cpp"
+
+int main() {
+
+    // создаем дерево с помощью списка инициализации
+    BinarySearchTree <int> tree ({1,2,3,4,5});
+
+    // читаем новый элемент дерева
+    std::cin >> tree;
+
+    // выводим дерево на печать
+    std::cout << tree << std::endl;
+
+    // количество узлов дерева
+    std::cout << "Range of tree - " << tree.size() << std::endl;
+
+    // использование контструктора копирования
+    BinarySearchTree <int> second = tree;
+    std::cout << second << std::endl;
+
+    // выводим дерево в файл
+    std::ofstream out("D:\\output.txt");
+    out << second;
+
+    std::getchar();
+    std::getchar();
+
+    return 0;
+}
