6a4d6bd46ddbf424c317d162f5337a08409fbd8a
A-Kod <andrey_kodyk@mail.ru>
2016-09-23 00:46:22 +0400

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -11,17 +11,29 @@ int main() {
 
     // читаем новый элемент дерева
     std::cin >> tree;
+    if (tree.find(3)==nullptr)
+        std::cout<<"not found"<<std::endl;
+    else std::cout<<tree.find(3)<<std::endl;
 
     // выводим дерево на печать
     std::cout << tree << std::endl;
 
-    // использование контструктора копирования
+    // количество узлов дерева
+    std::cout << "Range of first tree - " << tree.size() << std::endl;
+
+    // использование конструктора копирования
     BinarySearchTree <int> second = tree;
-    std::cout << second << std::endl;
 
     // выводим дерево в файл
     std::ofstream out("D:\\output.txt");
     out << second;
+
+    // использование контструктора перемещения
+    BinarySearchTree <int> third (std::move(second));
+    std::cout << third << std::endl;
+
+    // количество узлов 2 дерева после использования конструктора перемещения
+    std::cout << "Range of second tree - " << second.size() << std::endl;
 
     std::getchar();
     std::getchar();
