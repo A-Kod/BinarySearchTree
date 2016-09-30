a7b3a8852e899e2500a484f07ebc641741013baf
A-Kod <andrey_kodyk@mail.ru>
2016-09-30 17:54:35 +0400

    Update size.cpp

--- tests/size.cpp
+++ tests/size.cpp
@@ -7,23 +7,23 @@ SCENARIO( "size of empty tree must be equal 0" ) {
 
     GIVEN( "empty tree" ) 
     {
-        BinarySearchTree <int> tree;
+        BinarySearchTree <int> tree1;
 
         WHEN( "decide size" ) 
         {
 
             THEN( "size of empty tree must be equal 0" ) 
             {
-                REQUIRE( tree.size() == 0 );
+                REQUIRE( tree1.size() == 0 );
             }
         }
         WHEN( "new tree" ) 
         {
-            tree ({1,2,3,4,5});
+            BinarySearchTree <int> tree2 ({1,2,3,4,5});
 
             THEN( "the size changed" ) 
             {
-                REQUIRE( v.size() != 0 );
+                REQUIRE( tree2.size() != 0 );
             }
         }
         
@@ -33,8 +33,8 @@ SCENARIO( "size of empty tree must be equal 0" ) {
 SCENARIO("size of constant tree must be equal to size of non-constant tree") {
     GIVEN("constant and non-constant trees")
     {
-        const BinarySearchTree<int> tree1{1, 3, 5, 0};
-        BinarySearchTree<int> tree2{1, 3, 5, -1};
+        const BinarySearchTree<int> tree1({1, 2, 3, 4});
+        BinarySearchTree<int> tree2({5, 6, 7, 8});
         WHEN("compare sizes")
         {
             THEN("sizes must be equal")
