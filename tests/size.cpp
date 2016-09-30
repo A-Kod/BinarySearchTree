67bd13fc04fab556614fb314555e69d2492ce820
A-Kod <andrey_kodyk@mail.ru>
2016-09-30 17:44:59 +0400

    Update size.cpp

--- tests/size.cpp
+++ tests/size.cpp
@@ -1,1 +1,26 @@
+SCENARIO( "size of empty tree must be equal 0" ) {
 
+    GIVEN( "empty tree" ) 
+    {
+        BinarySearchTree <int> tree;
+
+        WHEN( "decide size" ) 
+        {
+
+            THEN( "size of empty tree must be equal 0" ) 
+            {
+                REQUIRE( tree.size() == 0 );
+            }
+        }
+        WHEN( "new tree" ) 
+        {
+            tree ({1,2,3,4,5});
+
+            THEN( "the size changed" ) 
+            {
+                REQUIRE( v.size() != 0 );
+            }
+        }
+        
+    }
+}
