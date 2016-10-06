2bdf15fd718156097de6049526c4692fd0778516
A-Kod <andrey_kodyk@mail.ru>
2016-10-06 07:56:29 +0400

    Update find.cpp

--- tests/find.cpp
+++ tests/find.cpp
@@ -15,10 +15,6 @@ SCENARIO("if obj is in tree")
             }
         }
     }
-}
-
-SCENARIO("if obj is not in tree") 
-{
     GIVEN("tree") 
     {
         BinarySearchTree<int> tree({1, 2, 3, 4, 5});
@@ -30,21 +26,16 @@ SCENARIO("if obj is not in tree")
             }
         }
     }
-}
-
-SCENARIO("find method must work for constant tree and non-constant tree") 
-{
-    GIVEN("constant and non-constant tree") 
+    GIVEN("constant tree") 
     {
         const BinarySearchTree<int> tree1({1, 2, 3});
-        BinarySearchTree<int> tree2{4, 5, 6};
         WHEN("find") 
         {
-            THEN("return element for constant and non-constant tree") 
+            THEN("return element for constant tree") 
             {
                 REQUIRE(tree1.find(1));
-                REQUIRE(tree2.find(4));
             }
         }
-    }
+    }    
 }
+
