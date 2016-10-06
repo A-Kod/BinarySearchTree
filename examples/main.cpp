7cc58e0450751413742d3b1615430e3d25cf3559
A-Kod <andrey_kodyk@mail.ru>
2016-10-06 07:49:04 +0400

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -9,36 +9,27 @@ int main() {
     // создаем дерево с помощью списка инициализации
     BinarySearchTree <int> tree ({1,2,3,4,5});
 
-    // читаем новый элемент дерева
-    std::cin >> tree;
+    // выводим дерево на печать
+     std::cout << tree << std::endl;
+
+
+    // количество узлов дерева
+    std::cout << "Range of tree - " << tree.size() << std::endl;
+
+    int del;
+    std::cout << "Input deleting element: ";
+    std::cin >> del;
+
+
+    if(tree.remove_el(del))
+    {
+        std::cout << "The element is deleted" << std::endl;
+    }
 
     // выводим дерево на печать
     std::cout << tree << std::endl;
+    std::cout << "Range of tree - " << tree.size() << std::endl;
 
-    // количество узлов дерева
-    std::cout << "Range of first tree - " << tree.size() << std::endl;
-
-    // читаем дерево из файла
-    BinarySearchTree <int> second;
-    std::ifstream in("D:\\input.txt");
-
-    in >> second;
-    std::cout << second << std::endl;
-
-    if(tree==second)
-        std::cout<<"Trees are equal"<<std::endl;
-
-    else std::cout<<"Trees are not equal" <<std::endl;
-
-
-    // использование контструктора перемещения
-    BinarySearchTree <int> third (std::move(second));
-    std::cout << third << std::endl;
-
-    // количество узлов 2 дерева после использования конструктора перемещения
-    std::cout << "Range of second tree - " << second.size() << std::endl;
-
-    std::getchar();
     std::getchar();
 
     return 0;
