743dc5ffa00c655173bb528cd34dca8a0a42adc7
A-Kod <andrey_kodyk@mail.ru>
2016-10-21 00:33:59 +0400

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -6,31 +6,57 @@
 
 int main() {
 
-    // создаем дерево с помощью списка инициализации
-    BinarySearchTree <int> tree ({5,2,1,3,6,7});
 
-    // выводим дерево на печать
-     std::cout << tree << std::endl;
+        // создаем дерево с помощью списка инициализации
+        BinarySearchTree<int> tree({5, 2, 1, 3, 6, 7});
 
-
-    // количество узлов дерева
-    std::cout << "Range of tree - " << tree.size() << std::endl;
-
-    int del;
-    std::cout << "Input deleting element: ";
-    std::cin >> del;
+        // выводим дерево на печать
+        std::cout << tree << std::endl;
 
 
-    if(tree.remove_helper(del))
-    {
-        std::cout << "The element is deleted" << std::endl;
-    }
+        // количество узлов дерева
+        std::cout << "Range of tree - " << tree.size() << std::endl;
 
-    // выводим дерево на печать
-    std::cout << tree << std::endl;
-    std::cout << "Range of tree - " << tree.size() << std::endl;
+        try {
+        tree.find(1);
+        }
+        catch (std::logic_error &e) {
+            std::cout << e.what();
+        }
 
-    std::getchar();
+        try {
+        tree.insert(9);
+        }
+
+        catch (std::logic_error &e) {
+        std::cout << e.what();
+        }
+
+        int del;
+        std::cout << "Input deleting element: ";
+        std::cin >> del;
+
+        try {
+        if (tree.remove_helper(del)) {
+            std::cout << "The element is deleted" << std::endl;
+        }
+        }
+
+        catch (std::logic_error &e) {
+        std::cout << e.what();
+        }
+
+        // выводим дерево на печать
+        std::cout << tree << std::endl;
+        std::cout << "Range of tree - " << tree.size() << std::endl;
+
+        std::getchar();
+
+
 
     return 0;
 }
+
+
+
+
